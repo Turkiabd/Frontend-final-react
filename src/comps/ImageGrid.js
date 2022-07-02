5e46e2a8fa593e9157f9635fb6ec5936e529c1d7
Turkiabd <102246247+Turkiabd@users.noreply.github.com>
2022-07-02 17:57:02 +0300

    delete-src-old

--- src/comps/ImageGrid.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import useFirestore from "../hooks/useFirestroe";
-
-const ImageGrid = () => {
-  return <div className="img-grid">Image</div>;
-};
-
-export default ImageGrid;
